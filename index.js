043dca96f312b54854ade53ea2be2981f8c10f69
devlitus <developercarles@gmail.com>
2021-08-04 08:56:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path');
+require('dotenv').config()
+
+const app = express();
+
+app.use(express.json());
+
+// Node Server
+const server = require('http').createServer(app);
+// Active server
+server.listen(process.env.PORT, (err) => {
+  if(err) throw new Error(err);
+  console.info(`Server up on port ${process.env.PORT}`);
+})
+
